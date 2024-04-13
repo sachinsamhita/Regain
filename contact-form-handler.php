e63927f27f3e025ae1f6acc56f5460688ce7ab8f
sachinsamhita <sachinsamhita@gmail.com>
2024-04-13 22:19:34 +0530

    mail

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'himanshududeja99@gmail.com';//<-----Put Your email address here.
+$myemail = 'sachinsamhita@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['phone']) || 
    empty($_POST['message']))
